322e7e14a9c376ca51850be5d0f04210bf9c3dfa
Daniel Oluojomu <danvixent@gmail.com>
2021-05-06 00:02:36 +0100

    add tests

---
+++ tests/handler_test.go
@@ -0,0 +1,96 @@
+package tests
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"testing"
+
+	"github.com/99designs/gqlgen/graphql"
+)
+
+func TestCalculatePrice(t *testing.T) {
+	const baseQuery = `query{
+		calculatePrice(type: %s,margin: %f,exchangeRate: %f)
+	}`
+
+	type args struct {
+		typeArg      string
+		margin       float64
+		exchangeRate float64
+	}
+	tests := []struct {
+		name     string
+		args     args
+		wantCode int
+		wantErr  bool
+	}{
+		{
+			name: "calculatePrice1",
+			args: args{
+				typeArg:      "buy",
+				margin:       0.2,
+				exchangeRate: 505,
+			},
+			wantCode: http.StatusOK,
+			wantErr:  false,
+		},
+		{
+			name: "calculatePrice2",
+			args: args{
+				typeArg:      "sell",
+				margin:       0.4,
+				exchangeRate: 484,
+			},
+			wantCode: http.StatusOK,
+			wantErr:  false,
+		},
+		{
+			name: "calculatePrice3",
+			args: args{
+				typeArg:      "sold",
+				margin:       0.4,
+				exchangeRate: 484,
+			},
+			wantCode: http.StatusUnprocessableEntity,
+			wantErr:  false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			query := fmt.Sprintf(baseQuery, tt.args.typeArg, tt.args.margin, tt.args.exchangeRate)
+			gql := graphql.RawParams{Query: query}
+
+			resp, err := calculatePrice(gql)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("calculatePrice() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if resp.StatusCode != tt.wantCode {
+				t.Errorf("calculatePrice() status code = %v, want %v", resp.StatusCode, tt.wantCode)
+			}
+		})
+	}
+}
+
+func calculatePrice(body interface{}) (*http.Response, error) {
+	return POST(baseURL+"/graphql", serialize(body))
+}
+
+// serialize obj into json bytes
+func serialize(obj interface{}) *bytes.Buffer {
+	buf := &bytes.Buffer{}
+	if err := json.NewEncoder(buf).Encode(obj); err != nil {
+		// if json encoding fails, stop the test immediately
+		log.Fatalf("unable to serialize obj: %v", err)
+	}
+	return buf
+}
+
+func POST(url string, body *bytes.Buffer) (*http.Response, error) {
+	return http.Post(url, "application/json", body)
+}
---
+++ tests/main_test.go
@@ -0,0 +1,53 @@
+package tests
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/danvixent/buycoins_challenge/graphql"
+	"github.com/danvixent/buycoins_challenge/handlers/margin"
+)
+
+const (
+	port    = "8081"
+	baseURL = "http://localhost:" + port
+)
+
+func TestMain(m *testing.M) {
+	marginHandler := margin.NewHandler()
+	graphqlHandler := graphql.NewHandler(marginHandler)
+
+	mux := http.NewServeMux()
+	graphqlHandler.SetupRoutes(mux)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+
+	log.Printf("serving graphql endpoint at http://localhost:%s/graphql", port)
+	log.Printf("serving graphiql endpoint at http://localhost:%s/graphiql", port)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Panicf("unable to listen: %s", err)
+		}
+	}()
+
+	// allow the routine above start the server
+	time.Sleep(time.Second)
+
+	// run the tests
+	code := m.Run()
+
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("unable to shutdown server gracefully: %v", err)
+	}
+
+	os.Exit(code)
+}
